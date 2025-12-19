caab0a92b604263a930d1d888edf56bdf79493e0
Dimitar <dimitarzankovit@gmail.com>
2025-12-19 11:36:28 +0200

    Added some test for recipe API

--- application/recipe/tests/test_recipe_api.py
+++ application/recipe/tests/test_recipe_api.py
@@ -10,23 +10,20 @@ from core import models
 from recipe import serializers
 
 
-RECIPES_URL = reverse('recipe:recipes-list')
+RECIPES_URL = reverse('recipes-list')
 
-
-def create_recipe(author,**params):
+def create_recipe(author,params):
 	# Create and return sample recipe
 	defaults = {
 	'title':'Sample Recipe',
 	'time_minutes':20,
+	'price':Decimal('5.50'),
 	'description':'Sample description',
+	'link':'http://example.com/recipe.pdf:'
 	}
 	defaults.update(params)
-	recipe = models.Recipes.objects.create(author=author,**defaults)
+	recipe = models.Recipe.objects.create(author=author,**defaults)
 	return recipe
-
-def detail_url(recipe_id):
-	# Create and return a recipe detail URL
-	return reverse('recipe:recipes-detail', args=[recipe_id])
 
 
 class PublicRecipeAPITests(TestCase):
@@ -35,66 +32,5 @@ class PublicRecipeAPITests(TestCase):
 		self.client = APIClient()
 	def test_auth_required(self):
 		# Test authentication is required to call API
-		result = self.client.get(RECIPES_URL)
-		self.assertEqual(result.status_code, status.HTTP_401_UNAUTHORIZED)
-
-class PrivateRecipeAPITests(TestCase):
-	# Test authenticated API requests
-	def setUp(self):
-		self.client = APIClient()
-		self.user = get_user_model().objects.create_user('user@example.com','examplepass123')
-		self.client.force_authenticate(self.user)
-
-	def test_retrieve_recipes(self):
-		# Test retrieving a list of recipes
-		create_recipe(author=self.user)
-		create_recipe(author=self.user)
-		result = self.client.get(RECIPES_URL)
-		recipes = models.Recipes.objects.all().order_by('-id')
-		serializer = serializers.RecipeSerializer(recipes,many=True)
-		self.assertEqual(result.status_code, status.HTTP_200_OK)
-		self.assertEqual(result.data,serializer.data)
-
-	def test_get_recipe_detail(self):
-		# Test get recipe detail
-		recipe = create_recipe(author=self.user)
-		url = detail_url(recipe.id)
-		result = self.client.get(url)
-		serializer = serializers.RecipeDetailSerializer(recipe)
-		self.assertEqual(result.data, serializer.data)
-
-	def test_create_recipe_with_new_tags(self):
-		# Test creating a recipe with new tags
-		payload = {
-			'title':'Thai Prawn Curry',
-			'time_minutes':30,
-			'tags':[{'name':'Thai'},{'name':'Dinner'}]
-		}
-		result = self.client.post(RECIPES_URL,payload, format='json')
-		self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-		recipes = models.Recipes.objects.filter(author=self.user)
-		self.assertEqual(recipes.count(), 1)
-		recipe = recipes[0]
-		self.assertEqual(recipe.tags.count(),2)
-		for tag in payload['tags']:
-			exists = recipe.tags.filter(name=tag['name'],author=self.user,).exists()
-			self.assertTrue(exists)
-
-	def test_create_recipe_with_existing_tags(self):
-		# Test creating a recipe with existing tag
-		tag_indian = models.Tag.objects.create(author=self.user,name='Indian')
-		payload = {
-			'title':'Pongal',
-			'time_minutes':60,
-			'tags':[{'name':'Indian'},{'name':'Breakfast'}],
-		}
-		result = self.client.post(RECIPES_URL, payload, format='json')
-		self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-		recipes = models.Recipes.objects.filter(author=self.user)
-		self.assertEqual(recipes.count(), 1)
-		recipe = recipes[0]
-		self.assertEqual(recipe.tags.count(), 2)
-		self.assertIn(tag_indian, recipe.tags.all())
-		for tag in payload['tags']:
-			exists = recipe.tags.filter(name=tag['name'],author=self.user,).exists()
-			self.assertTrue(exists)
+		res = self.client.get(RECIPES_URL)
+		self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
